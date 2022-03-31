0db5bfd50043ad80fc0b409a8897c687722eec10
Remnant <georgesimon4801@gmail.com>
2022-03-31 13:11:26 -0400

    mvc

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,12 +6,17 @@ router.get('/', (req, res) => {
     User.findAll({
       attributes: { exclude: ['password'] }
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    // session info for routes
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
       });
-  });;
+    })
+  });
 
   router.get('/:id', (req, res) => {
     User.findOne({
@@ -68,7 +73,6 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -85,9 +89,28 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
+      // saves session
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
+});
+
+// logout logic
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 router.put('/:id', (req, res) => {
